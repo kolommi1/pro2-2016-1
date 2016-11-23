ff8fe38065f67a68fb4123c0d482fba5f94bd9b5
kolommi1 <kolommi1@J09-10.uhk.cz>
2016-11-23 15:43:58 +0100

    Přidání argumentů do funkcí interface

--- src/cz/uhk/fim/pro2/game/interfacer/WorldListener.java
+++ src/cz/uhk/fim/pro2/game/interfacer/WorldListener.java
@@ -1,9 +1,12 @@
 package cz.uhk.fim.pro2.game.interfacer;
 
+import cz.uhk.fim.pro2.game.model.Heart;
+import cz.uhk.fim.pro2.game.model.Tube;
+
 public interface WorldListener {
-	void crashTube();
+	void crashTube(Tube tube);
 
 	void outOf();
 
-	void catchHeart();
+	void catchHeart(Heart heart);
 }
