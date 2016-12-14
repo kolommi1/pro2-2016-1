e222aa164e15801eb0a31923656bfd89f3f541b1
kolommi1 <kolommi1@J09-11.uhk.cz>
2016-12-14 16:29:59 +0100

    Ukládání, čtení score do/ze souboru.

--- src/cz/uhk/fim/pro2/game/Game.java
+++ src/cz/uhk/fim/pro2/game/Game.java
@@ -1,13 +1,26 @@
 package cz.uhk.fim.pro2.game;
 
+import java.io.File;
+import java.io.IOException;
+
 import cz.uhk.fim.pro2.game.gui.HomeScreen;
 import cz.uhk.fim.pro2.game.gui.MainFrame;
 
-
 public class Game {
 
+	public static final String SCORE_FILE = "scores.csv";
+	
 	public static void main(String[] args) {
 
+		File file = new File(SCORE_FILE);
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		MainFrame mainFrame = new MainFrame();
 		
 		HomeScreen homeScreen= new HomeScreen(mainFrame);
