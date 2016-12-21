83ff92b6e467eec557d3a509a1b0754b5366c4e3
kolommi1 <kolommi1@J09-11.uhk.cz>
2016-12-21 16:14:41 +0100

    Oprava blikání, posouvání pozadí

--- src/cz/uhk/fim/pro2/game/gui/GameCanvas.java
+++ src/cz/uhk/fim/pro2/game/gui/GameCanvas.java
@@ -1,6 +1,5 @@
 package cz.uhk.fim.pro2.game.gui;
 
-import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -9,13 +8,14 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 import cz.uhk.fim.pro2.game.model.Bird;
 import cz.uhk.fim.pro2.game.model.Heart;
 import cz.uhk.fim.pro2.game.model.Tube;
 import cz.uhk.fim.pro2.game.model.World;
 
-public class GameCanvas extends Canvas {
+public class GameCanvas extends JPanel {
 	private World world;
 	
 	private BufferedImage imageBird, imageHeart, imageBackground, imageBottom, imageTube, imageTop;
@@ -23,11 +23,14 @@ public class GameCanvas extends Canvas {
 	public static int UP_BOUND = 100;
 	public static int DOWN_BOUND = 70;
 	
+	private int background_x = 0, background_x2 = MainFrame.WIDTH;
+	private static int BACKGROUND_LIMIT = 0-MainFrame.WIDTH;
+	
 	
 	public GameCanvas(World world) {
 		this.world=world;
 		
-		try {
+		try {		
 			imageBird = ImageIO.read(new File("assets/bird.png"));
 			imageHeart = ImageIO.read(new File("assets/heart.png"));
 			imageBackground = ImageIO.read(new File("assets/background.png"));
@@ -40,11 +43,20 @@ public class GameCanvas extends Canvas {
 	}
 	
 	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
 		g.setColor(Color.cyan);
-		g.drawImage(imageBackground,0, UP_BOUND, MainFrame.WIDTH, MainFrame.HEIGHT-DOWN_BOUND,null);
+		g.drawImage(imageBackground,background_x, UP_BOUND, MainFrame.WIDTH, MainFrame.HEIGHT-DOWN_BOUND,null);
+		g.drawImage(imageBackground,background_x2, UP_BOUND, MainFrame.WIDTH, MainFrame.HEIGHT-DOWN_BOUND,null);
+		background_x--;
+		if(background_x<BACKGROUND_LIMIT){
+			background_x = MainFrame.WIDTH-10;
+		}		
+		background_x2--;
+		if(background_x2<BACKGROUND_LIMIT){
+			background_x2 = MainFrame.WIDTH-10;
+		}
 		
 		Bird bird = world.getBird();
 		bird.paint(g,imageBird);
--- src/cz/uhk/fim/pro2/game/gui/GameScreen.java
+++ src/cz/uhk/fim/pro2/game/gui/GameScreen.java
@@ -5,7 +5,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.Timer;
@@ -111,7 +115,7 @@ public class GameScreen extends Screen implements WorldListener {
 					mainFrame.setScreen(new FinishScreen(mainFrame,world));
 				}
 				
-				gamecanvas.repaint();
+				repaint();
 				
 				lastTimeMillis = currentTimeMillis;
 			}
@@ -138,6 +142,16 @@ public class GameScreen extends Screen implements WorldListener {
 		bird.setSpeed(Bird.JUMP / 2);
 		
 		bird.removeLife();
+		try {			
+			File soundFile = new File("assets/hit.wav");
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);			
+			Clip clip = AudioSystem.getClip();		
+			clip.open(audioInputStream);
+			
+			clip.start();		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
